b54dee8e01e8ffcb9323cc1241adb1050e50ff5e
Vesko Vujovic <veskovujovic@yahoo.com>
2015-07-17 21:18:44 +0200

    last changes to list function
    finished lists

--- lists.js
+++ lists.js
@@ -8,21 +8,21 @@ function List(){
     this.pos         = 0;
     // initialize  empty array to store our values
     this.dataStore   = [];
-    //this.clear       = clear;
+    this.clear       = clear;
     this.find        = find;
     this.toString    = toString;
-    //this.insert      = insert;
+    this.insert      = insert;
     this.append      = append;
     this.remove      = remove;
-    //this.front       = front;
-    //this.end         = end;
-    //this.prev        = prev;
-    //this.next        = next;
+    this.front       = front;
+    this.end         = end;
+    this.prev        = prev;
+    this.next        = next;
     this.length      = length;
-    //this.currPos     = currPos;
+    this.currPos     = currPos;
     this.moveTo      = moveTo;
-    //this.getElement  = getElement;
-    //this.contains    = contains;
+    this.getElement  = getElement;
+    this.contains    = contains;
 }
 
 // After we append element the size is incremented
@@ -56,12 +56,79 @@ function remove(element){
 function length(){
     return this.listSize;
 }
+// Returns all elemnts of the list
 function toString(){
     return this.dataStore;
 }
 
+// Inserts elemnt in specified position
+function insert(element, after){
+    var insertPos = this.find(after);
+    if(insertPos > -1){
+        this.dataStore.splice(insertPos+1, 0, element);
+        ++this.listSize;
+        return true;
+    }
+    return false;
+
+}
+
+function contains(element){
+    for(var i = 0; i < this.dataStore.length; ++i){
+        if(this.dataStore[i] == element){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Remove all elements from the list
+function clear(){
+    delete this.dataStore;
+    this.dataStore = [];
+    this.listSize = this.pos = 0;
+}
+
+function front(){
+    this.pos = 0;
+}
+
+function end(){
+    this.pos = this.listSize - 1;
+}
+
+function prev(){
+    if(this.pos > 0){
+        --this.pos;
+    }
+}
+
+function next(){
+    if(this.pos < this.listSize - 1){
+        ++this.pos;
+    }
+}
+
+function currPos(){
+    return this.pos;
+}
+
+function moveTo(position){
+    this.pos = position;
+}
+
+function getElement(){
+    return this.dataStore[this.pos];
+}
+/**
+ * Iteration over this list can be done like this
 
 
+for(names.front(); names.currPos() < names.length(); names.next()){
+     //assign   names.getElement() to variable
+}
+
+/*
 
 
 
